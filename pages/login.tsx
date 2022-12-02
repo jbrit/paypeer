82140231e0b38ea5739c794158b2bfba006f453f
jibola <pro.ajibolaojo@gmail.com>
2022-12-02 05:22:29 +0100

    auth UI

---
+++ pages/login.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Link from "next/link";
+import { Button, Checkbox, Form, Input, Row, Col, Typography } from "antd";
+
+export default function Login() {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Paypeer | Login</title>
+        <meta name="description" content="Paypeer  Login Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Row>
+        <Col span={8} offset={8}>
+          <Typography.Title level={2} style={{paddingTop: "5rem", textAlign: "center"}}>
+            <span>Paypeer</span><span style={{fontWeight: 400}}> | Login</span>
+          </Typography.Title>
+          <p style={{textAlign: "center", marginBottom: "3rem", fontSize: "1rem"}}>Not registered? <Link href ="/register">Register</Link></p>
+          <Form
+            name="basic"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            initialValues={{}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Please input your email!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ span: 24 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+}
---
+++ pages/register.tsx
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import Link from "next/link";
+import { Button, Checkbox, Form, Input, Row, Col, Typography } from "antd";
+
+export default function Register() {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Paypeer | Register</title>
+        <meta name="description" content="Paypeer Registeration Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Row>
+        <Col span={8} offset={8}>
+          <Typography.Title level={2} style={{paddingTop: "5rem", textAlign: "center"}}>
+            <span>Paypeer</span><span style={{fontWeight: 400}}> | Register</span>
+          </Typography.Title>
+          {/* Already registered here */}
+          <p style={{textAlign: "center", marginBottom: "3rem", fontSize: "1rem"}}>Already registered? <Link href="/login">Login</Link></p>
+          <Form
+            name="basic"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            initialValues={{}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                { required: true, message: "Please input your name!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Please input your email!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ span: 24 }}>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+}
